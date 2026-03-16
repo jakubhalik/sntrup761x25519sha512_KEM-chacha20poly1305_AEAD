66764f20f68c4f30da68e470c5b4c6b6c19c0c4b
Jakub Halik <oranzovydzusaja@proton.me>
2026-03-16 17:55:05 +0100

    just arg name

--- src/client/args.rs
+++ src/client/args.rs
@@ -3,5 +3,5 @@ use crate::define_args;
 define_args! {
     mess_test_without_auth: &["--mess_test_without_auth", "--messtestwithoutauth"],
     proxy: &["--proxy"],
-    r: &["-R", "--reverse", "--reverse_tunnel", "--reversetunnel", "--reversetnl", "--reverse_tnl", "--rvrstnl", "--reverse_tunel", "--reversetunel"],
+    reverse_tunnel: &["-R", "--reverse", "--reverse_tunnel", "--reversetunnel", "--reversetnl", "--reverse_tnl", "--rvrstnl", "--reverse_tunel", "--reversetunel"],
 }
